f0031825e905c2cb16e0b30e7d3bfc34f671251f
iam-Mai <sasithorn.senmaithai@gmail.com>
2020-11-22 01:31:45 +0700

    Fix graph

--- src/P0/C2.java
+++ src/P0/C2.java
@@ -7,8 +7,5 @@ public class C2 {
         System.out.println("call P0.C2");
         C3 c3 = new C3();
         c3.callC3();
-
-        C13 c13 = new C13();
-        c13.callC13();
     }
 }
--- src/P0/C6.java
+++ src/P0/C6.java
@@ -3,7 +3,5 @@ package P0;
 public class C6 {
     public void callC6() {
         System.out.println("call P0.C6");
-        C7 c7 = new C7();
-        c7.callC7();
     }
 }
